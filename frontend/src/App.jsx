9573c3aa32b66c89c36a232cb6f76fab05bcabda
kevin <kevinribadiya18@gmail.com>
2026-03-04 16:09:11 +0530

    setup react vite with tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-2xl text-blue-400'>Hello from frontend..</h1>
+    </>
+  )
+}
+
+export default App
